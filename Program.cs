30071bdc09b60806ee0642111e7153e592c8ade9
OMAR MORENO MELENDEZ <melendezstudio71@gmail.com>
2025-07-01 10:54:20 -0400

    emprendelocal

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using EmprendeLocal.Components;
-
+// se utiliza la biblioteca de componentes de Razor para crear aplicaciones web interactivas y reactivas.
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,12 +16,14 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//Redirige automáticamente todas las solicitudes HTTP a HTTPS.
 app.UseHttpsRedirection();
-
+// Permite el uso de archivos estáticos, como CSS, JavaScript e imágenes en la carpeta "wwwroot" del proyecto.
 app.UseStaticFiles();
 app.UseAntiforgery();
-
+// Permite el uso de componentes de Razor en la aplicación.
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+// Finalmente, arranca la aplicación web. Desde aquí, la app comienza a escuchar peticiones HTTP y
+// a responderlas según la configuración y los componentes definidos anteriormente.
 app.Run();
